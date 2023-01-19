b90e98735973596f19a9efce9f6ed05ae692f5a6
brianzzzasd <repuestobrian2@gmail.com>
2023-01-19 10:21:03 +0800

    Add .env on frontend

--- frontend/src/plugins/fetch.ts
+++ frontend/src/plugins/fetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const fetch = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
